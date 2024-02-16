0c2084fe34cb4d5415f6e5c29220a028c0064aa6
DreamsForest <muhinv451@gmail.com>
2024-02-16 15:03:31 +0300

    1.1

--- Лабораторная 1/index.php
+++ Лабораторная 1/index.php
@@ -1,39 +1,18 @@
 <!DOCTYPE html>
-<!-- saved from url=(0057)https://getbootstrap.com/docs/5.1/examples/navbar-static/ -->
+<?php ?>
 <html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.88.1">
-    <title>Лабораторная 1</title>
+    <title>Образование в Швейцарии | Среднее и высшее образование |Условия поступления и обучения | Спецпроект Forbes Education</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/navbar-static/">
-
+    <link href="index.css" rel="stylesheet">
     
 
     <!-- Bootstrap core CSS -->
 <link href="./Top navbar example · Bootstrap v5.1_files/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-
-
-
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
-
     
     <!-- Custom styles for this template -->
     <link href="./Top navbar example · Bootstrap v5.1_files/navbar-top.css" rel="stylesheet">
